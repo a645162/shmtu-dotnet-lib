0662ebe27e61c2bf6e402dbe37b3bb27c9d04828
Haomin Kong <a645162@qq.com>
2024-06-03 02:24:39 +0800

    Create Project

---
+++ shmtu-cas-dotnet-demo/Program.cs
@@ -0,0 +1,6 @@
+﻿using shmtu_cas_dotnet;
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("ShangHai Maritime University CAS .NET Library Demo");
+Console.WriteLine($"Library Version: {Shmtu_Cas_Dotnet.version}");
---
+++ shmtu-cas-dotnet/Shmtu_Cas_Dotnet.cs
@@ -0,0 +1,6 @@
+﻿namespace shmtu_cas_dotnet;
+
+public class Shmtu_Cas_Dotnet
+{
+    public const string version = "1.0.0";
+}
